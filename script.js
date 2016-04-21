0bdc6ec270a384b4aa1ec2ffb6503b68e39458a7
Jon Rasmussen <jonathan.a.rasmussen@gmail.com>
2016-04-21 00:16:48 -0700

    much closer, just need timer

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 /**
  * Created by jar4677 on 4/20/16.
  */
+
+//declare global variables
+var firstCardClicked = null;
+var secondCardClicked = null;
+var totalPossibleMatches = 2;
+var matchCounter = 0;
+
 //initializing jQuery
 $(document).ready(function () {
 
-    //declare global variables
-    var first_card_clicked = null;
-    var second_card_clicked = null;
-    var total_possible_matches = 2;
-    var match_counter = 0;
-    
-    function card_clicked() {
-        $(this).toggleClass('flip');
-    }
-    
-    $(".card").on("click", "div", card_clicked)
+    function cardClicked() {
 
+        //function to flip card
+        function flipCard(x) {
+            $(x).children("div").toggleClass('down');
+        }
+
+        //checks to see if card is already face down. prevents re-flipping cards
+        if ($(this).children(".front").hasClass('down')) {
+
+            flipCard(this);
+
+            if (firstCardClicked == null) {
+                firstCardClicked = this;
+            } else {
+                secondCardClicked = this;
+                if ($(firstCardClicked).html() == $(secondCardClicked).html()) {
+                    matchCounter++;
+                    firstCardClicked = null;
+                    secondCardClicked = null;
+                    if (matchCounter == totalPossibleMatches) {
+                        $("#victory").toggleClass('victory');
+                    }
+                } else {
+                    flipCard(firstCardClicked);
+                    flipCard(secondCardClicked);
+                    firstCardClicked = null;
+                    secondCardClicked = null;
+                }
+            }
+        }
+    }
+
+    $("#game-area").on('click', ".card", cardClicked);
 
 });
